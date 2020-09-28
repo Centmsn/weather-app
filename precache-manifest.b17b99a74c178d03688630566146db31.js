34ff4857b993574facd198d68c34be643c2d963d
Wojtek Rygorowicz <wojciech.rygorowicz@gmail.com>
2020-09-28 16:12:32 +0200

    Updates

---
+++ precache-manifest.b17b99a74c178d03688630566146db31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c90699bc5fc9938b401b504f9acb0528",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "0c99e46f6306b1921344",
+    "url": "/weather-app/static/css/main.bdc0ba59.chunk.css"
+  },
+  {
+    "revision": "f7cfcf6e15ab3e8a2c58",
+    "url": "/weather-app/static/js/2.4c2778af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.4c2778af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c99e46f6306b1921344",
+    "url": "/weather-app/static/js/main.eb418573.chunk.js"
+  },
+  {
+    "revision": "bdcb51b63d9249268df7",
+    "url": "/weather-app/static/js/runtime-main.f1c906e7.js"
+  },
+  {
+    "revision": "fed6986c96fa5858b47171ae05b3248a",
+    "url": "/weather-app/static/media/rain.fed6986c.mp4"
+  }
+]);
